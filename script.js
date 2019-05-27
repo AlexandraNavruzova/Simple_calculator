3446d0f33982527e570862ea596ec5cfe10c5b9e
Alexandra Navruzova <45698556+AlexandraNavruzova@users.noreply.github.com>
2019-05-27 14:26:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const btn = document.querySelector('button');
 btn.addEventListener('click', function sumValues(){
   let a = Number(document.querySelector('#first-number').value);
   let b = Number(document.querySelector('#second-number').value);
-  let result = (a + b).toFixed(1);
+  let result = +(a + b).toFixed(1);
 
   if (!isNaN(result)) {
     alert(`Сумма первого и второго числа: ${result}`);
